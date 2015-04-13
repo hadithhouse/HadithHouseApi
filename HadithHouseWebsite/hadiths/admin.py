07a633e9d0a37d5be4df9d1d1ccf533288a0a8a7
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2015-04-13 22:38:10 +0100

    Registering Person, Hadith, and HadithTag to show in the admin pages.
    #7

--- HadithHouseWebsite/hadiths/admin.py
+++ HadithHouseWebsite/hadiths/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from hadiths.models import Hadith, Person, HadithTag
 
-# Register your models here.
+admin.site.register(Hadith)
+admin.site.register(Person)
+admin.site.register(HadithTag)
