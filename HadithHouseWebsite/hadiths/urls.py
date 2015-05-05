5d6a07bee1a510645ba4ccc195dbec735ac5607a
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2015-05-05 03:15:18 +0100

    Adding simple pages to view hadith(s).
    - A page to view listings of hadiths.
    - A page to view details of a single hadith.
    
    The pages are very simple for now and they need to be improved.
    
    #4

--- HadithHouseWebsite/hadiths/urls.py
+++ HadithHouseWebsite/hadiths/urls.py
@@ -5,4 +5,6 @@ from hadiths import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^persons$', views.persons, name='persons'),
+    url(r'^hadith/(?P<hadith_id>\d+)$', views.hadith, name='hadith'),
+    url(r'^hadiths$', views.hadiths, name='hadiths'),
 )
--- HadithHouseWebsite/hadiths/views.py
+++ HadithHouseWebsite/hadiths/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from hadiths.models import Person
+
+from hadiths.models import Person, Hadith
 
 
 def index(request):
@@ -15,6 +16,29 @@ def persons(request):
   template = loader.get_template('hadiths/persons.html')
   context = RequestContext(request, {
     'persons': Person.objects.all()
+  })
+  return HttpResponse(template.render(context))
+
+
+def hadith(request, hadith_id):
+  template = loader.get_template('hadiths/hadith.html')
+  hadith = Hadith.objects.get(id=hadith_id)
+  chains = [{'links': [
+    {
+      'person_name': link.person.full_name
+    } for link in chain.chainlink_set.all()
+  ]} for chain in hadith.chain_set.all()]
+  context = RequestContext(request, {
+    'hadith': hadith,
+    'chains': chains
+  })
+  return HttpResponse(template.render(context))
+
+
+def hadiths(request):
+  template = loader.get_template('hadiths/hadiths.html')
+  context = RequestContext(request, {
+    'hadiths': Hadith.objects.all()
   })
   return HttpResponse(template.render(context))
 
