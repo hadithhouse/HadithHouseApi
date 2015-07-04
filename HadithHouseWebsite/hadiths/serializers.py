f109ecd1b1cde48ba8d021a157b2e9127d487295
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2015-07-05 00:09:37 +0100

    Manually specify the datetime format in HadithTagSerializer
    This is because for some reason the format returned by POST requests is
    different to the one retrieved by GET requests. See this for more
    information:
    
    http://stackoverflow.com/questions/31225467/generics-retrieveupdatedestroyapiview-and-generics-listcreateapiview-format-date

--- HadithHouseWebsite/hadiths/serializers.py
+++ HadithHouseWebsite/hadiths/serializers.py
@@ -16,6 +16,13 @@ class HadithTagSerializer(serializers.ModelSerializer):
     model = HadithTag
     fields = ['name', 'added_on', 'updated_on']
 
+  # Manually specify the format of added_on and updated_on because otherwise for some
+  # reason the format returned by POST requests is different to the one retrieved
+  # by GET requests. See this for more information:
+  # http://stackoverflow.com/questions/31225467/generics-retrieveupdatedestroyapiview-and-generics-listcreateapiview-format-date
+  added_on = serializers.DateTimeField(read_only=True, format='%Y-%m-%dT%H:%M:%SZ')
+  updated_on = serializers.DateTimeField(read_only=True, format='%Y-%m-%dT%H:%M:%SZ')
+
 
 class HadithSerializer(serializers.ModelSerializer):
   tags = serializers.PrimaryKeyRelatedField(many=True, queryset=HadithTag.objects.all(), required=False)
