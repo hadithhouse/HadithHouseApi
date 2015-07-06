424117dec6938c283066cea339068b6358a2ab88
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2015-07-06 01:27:00 +0100

    Manually specify the datetime format in PersonSerializer and HadithSerializer
    This is because for some reason the format returned by POST requests is
    different to the one retrieved by GET requests. See this for more
    information:
    
    http://stackoverflow.com/questions/31225467/generics-retrieveupdatedestroyapiview-and-generics-listcreateapiview-format-date

--- HadithHouseWebsite/hadiths/serializers.py
+++ HadithHouseWebsite/hadiths/serializers.py
@@ -9,6 +9,12 @@ class PersonSerializer(serializers.ModelSerializer):
               'birth_year', 'birth_month', 'birth_day',
               'death_year', 'death_month', 'death_day',
               'added_on', 'updated_on']
+  # Manually specify the format of added_on and updated_on because otherwise for some
+  # reason the format returned by POST requests is different to the one retrieved
+  # by GET requests. See this for more information:
+  # http://stackoverflow.com/questions/31225467/generics-retrieveupdatedestroyapiview-and-generics-listcreateapiview-format-date
+  added_on = serializers.DateTimeField(read_only=True, format='%Y-%m-%dT%H:%M:%SZ')
+  updated_on = serializers.DateTimeField(read_only=True, format='%Y-%m-%dT%H:%M:%SZ')
 
 
 class HadithTagSerializer(serializers.ModelSerializer):
@@ -25,8 +31,14 @@ class HadithTagSerializer(serializers.ModelSerializer):
 
 
 class HadithSerializer(serializers.ModelSerializer):
-  tags = serializers.PrimaryKeyRelatedField(many=True, queryset=HadithTag.objects.all(), required=False)
-
   class Meta:
     model = Hadith
     fields = ['id', 'text', 'person', 'tags', 'added_on', 'updated_on']
+
+  tags = serializers.PrimaryKeyRelatedField(many=True, queryset=HadithTag.objects.all(), required=False)
+  # Manually specify the format of added_on and updated_on because otherwise for some
+  # reason the format returned by POST requests is different to the one retrieved
+  # by GET requests. See this for more information:
+  # http://stackoverflow.com/questions/31225467/generics-retrieveupdatedestroyapiview-and-generics-listcreateapiview-format-date
+  added_on = serializers.DateTimeField(read_only=True, format='%Y-%m-%dT%H:%M:%SZ')
+  updated_on = serializers.DateTimeField(read_only=True, format='%Y-%m-%dT%H:%M:%SZ')
