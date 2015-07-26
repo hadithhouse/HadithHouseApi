32ae8d46e159dce758e83c596fb5ed1bd17079e2
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2015-07-26 10:55:35 +0100

    Disabling auth and admin apps.
    Because we are currently depending on Facebook authentication. We can
    re-enable it later if required.

--- HadithHouseWebsite/HadithHouseWebsite/settings.py
+++ HadithHouseWebsite/HadithHouseWebsite/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
+    #'django.contrib.admin',
+    #'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
--- HadithHouseWebsite/HadithHouseWebsite/urls.py
+++ HadithHouseWebsite/HadithHouseWebsite/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+#from django.contrib import admin
 
 urlpatterns = patterns(
   '', # No prefix
-  url(r'^', include('hadiths.urls')),
-  url(r'^admin/', include(admin.site.urls)),
+  url(r'^', include('hadiths.urls'))
+  #url(r'^admin/', include(admin.site.urls)),
 )
