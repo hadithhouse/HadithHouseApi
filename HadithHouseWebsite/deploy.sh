5c1b0ae1883b0b6828dea5d30ca3d5deecc187da
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2015-11-11 20:02:21 +0000

    Refactoring and commenting the deployment script.
    #41

--- HadithHouseWebsite/deploy.sh
+++ HadithHouseWebsite/deploy.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+SERVER_SETTINGS_PATH='/home/jenkins'
+DEPLOYMENT_PATH='/var/www/HadithHouseWebsite'
+
+# Stops the execution of the script if any command, including pipes, fail.
 set -e 
 set -o pipefail
 
-echo "Deleting /var/www/HadithHouseWebsite/"
-rm -rf /var/www/HadithHouseWebsite/*
+# Deleting the current files in the deployment directory.
+echo "Deleting ${DEPLOYMENT_PATH}"
+rm -rf ${DEPLOYMENT_PATH}/*
 
-echo "Copy server_settings.py from /home/jenkins/server_settings.py to /var/www/HadithHouseWebsite/"
-cp /home/jenkins/server_settings.py HadithHouseWebsite/
+# Copy server settings file into te build directory.
+echo "Copy server_settings.py from $SERVER_SETTINGS_PATH to `pwd`/HadithHouseWebsite/"
+cp ${SERVER_SETTINGS_PATH}/server_settings.py HadithHouseWebsite/
 
+# Collect Django's static files.
 echo "Running manage.py collectstatic to collect static files."
 python manage.py collectstatic --noinput
 
-echo "Copying /tmp/hadithhouse/HadithHouseWebsite to /var/www/HadithHouseWebsite"
-cp -r ./* /var/www/HadithHouseWebsite/
+# Copy the project onto the deployment directory.
+echo "Copying `pwd`/* to $DEPLOYMENT_PATH"
+cp -r ./* ${DEPLOYMENT_PATH}/
 
+# Restart Apache2 server.
 echo "Restarting Apache2 server"
 sudo /usr/bin/service apache2 restart
+
