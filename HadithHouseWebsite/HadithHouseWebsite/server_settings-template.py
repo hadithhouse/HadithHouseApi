4405d95941a801324719a60a1586d3ce878bb08a
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2015-11-22 13:58:23 +0000

    Adding SETUP.txt and server_settings-template.py.
    SETUP.txt: Contains instructions on how to setup the project. I added
    instrurctions for Windows only at the moment, but we need to add
    instructions for Linux and Mac later.

---
+++ HadithHouseWebsite/HadithHouseWebsite/server_settings-template.py
@@ -0,0 +1,62 @@
+# IMPORTANT NOTE: This file contains sensitive information. NEVER commit it
+# to the repository, only upload it to the server.
+
+ENVIRONMENT = 'local'
+
+
+def get_debug():
+  if ENVIRONMENT == 'local':
+    return True
+  else:
+    return False
+
+
+def get_allowed_hosts():
+  if ENVIRONMENT == 'local':
+    return [
+      '*'
+    ]
+  else:
+    # Add the list of domain names where you are hosting the website.
+    return [
+    ]
+
+
+def get_fb_appid():
+  if ENVIRONMENT == 'local':
+    # Development application
+    return '<ID for development Facebook App.'
+  else:
+    # Production application
+    return '<ID for production Facebook App.'
+
+
+def get_db_settings():
+  return get_dev_settings()
+
+
+def get_local_db_settings():
+  return {
+    'default': {
+      'ENGINE': '<mysql.connector.django | django.db.backends.mysql>',
+      'NAME': 'HadithHouseLocal',
+      'USER': '<The username for your MySQL server>',
+      'PASSWORD': '<The password for your username>',
+      'HOST': '<The address of your MySQL server>',
+      'PORT': '<The port of your MySQL server>',
+    }
+  }
+
+
+def get_dev_settings():
+  return {
+    'default': {
+      'ENGINE': '<mysql.connector.django | django.db.backends.mysql>',
+      'NAME': 'HadithHouseDev',
+      'USER': '<The username for your MySQL server>',
+      'PASSWORD': '<The password for your username>',
+      'HOST': '<The address of your MySQL server>',
+      'PORT': '<The port of your MySQL server>',
+    }
+  }
+
