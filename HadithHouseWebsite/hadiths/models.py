062b58a735c79feb46fb21e26a90ce7e19e165f1
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2016-03-03 01:40:22 +0000

    Fixing a bug with `simplify_arabic_text` function
    If the input is `None`, the function or one of its callees throws
    an error. This commit fixes this problem.

--- HadithHouseWebsite/hadiths/models.py
+++ HadithHouseWebsite/hadiths/models.py
@@ -147,14 +147,20 @@ class FbUser(models.Model):
 
 
 def remove_arabic_diactrictics(input):
+  if input is None:
+    return None
   return re.sub(u'[\u064B-\u0652]', '', input, flags=re.MULTILINE)
 
 
 def unify_alif_letters(input):
+  if input is None:
+    return None
   return re.sub(u'[\u0622\u0623\u0625]', u'\u0627', input, flags=re.MULTILINE)
 
 
 def simplify_arabic_text(input):
+  if input is None:
+    return None
   return unify_alif_letters(remove_arabic_diactrictics(input))
 
 
