518d0b7e6f683f8e56890d5d6fbc878d0fda8c00
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2016-04-30 13:05:17 +0100

    feat(db): Use 2-space indentation instead 8

--- PostgresBackup/pg_backup.sh
+++ PostgresBackup/pg_backup.sh
@@ -5,26 +5,26 @@
 ###########################
 
 while [ $# -gt 0 ]; do
-        case $1 in
-                -c)
-                        if [ -r "$2" ]; then
-                                source "$2"
-                                shift 2
-                        else
-                                ${ECHO} "Unreadable config file \"$2\"" 1>&2
-                                exit 1
-                        fi
-                        ;;
-                *)
-                        ${ECHO} "Unknown Option \"$1\"" 1>&2
-                        exit 2
-                        ;;
-        esac
+  case $1 in
+    -c)
+      if [ -r "$2" ]; then
+        source "$2"
+        shift 2
+      else
+        ${ECHO} "Unreadable config file \"$2\"" 1>&2
+        exit 1
+      fi
+      ;;
+    *)
+      ${ECHO} "Unknown Option \"$1\"" 1>&2
+      exit 2
+      ;;
+  esac
 done
 
 if [ $# = 0 ]; then
-        SCRIPTPATH=$(cd ${0%/*} && pwd -P)
-        source $SCRIPTPATH/pg_backup.config
+  SCRIPTPATH=$(cd ${0%/*} && pwd -P)
+  source $SCRIPTPATH/pg_backup.config
 fi;
 
 ###########################
@@ -33,8 +33,8 @@ fi;
 
 # Make sure we're running as the required backup user
 if [ "$BACKUP_USER" != "" -a "$(id -un)" != "$BACKUP_USER" ]; then
-        echo "This script must be run as $BACKUP_USER. Exiting." 1>&2
-        exit 1;
+  echo "This script must be run as $BACKUP_USER. Exiting." 1>&2
+  exit 1;
 fi;
 
 
@@ -43,11 +43,11 @@ fi;
 ###########################
 
 if [ ! $HOSTNAME ]; then
-        HOSTNAME="localhost"
+  HOSTNAME="localhost"
 fi;
 
 if [ ! $USERNAME ]; then
-        USERNAME="postgres"
+  USERNAME="postgres"
 fi;
 
 
@@ -61,8 +61,8 @@ FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`/"
 echo "Making backup directory in $FINAL_BACKUP_DIR"
 
 if ! mkdir -p $FINAL_BACKUP_DIR; then
-        echo "Cannot create backup directory in $FINAL_BACKUP_DIR. Go and fix it!" 1>&2
-        exit 1;
+  echo "Cannot create backup directory in $FINAL_BACKUP_DIR. Go and fix it!" 1>&2
+  exit 1;
 fi;
 
 
@@ -72,7 +72,7 @@ fi;
 
 for SCHEMA_ONLY_DB in ${SCHEMA_ONLY_LIST//,/ }
 do
-        SCHEMA_ONLY_CLAUSE="$SCHEMA_ONLY_CLAUSE or datname ~ '$SCHEMA_ONLY_DB'"
+  SCHEMA_ONLY_CLAUSE="$SCHEMA_ONLY_CLAUSE or datname ~ '$SCHEMA_ONLY_DB'"
 done
 
 SCHEMA_ONLY_QUERY="select datname from pg_database where false $SCHEMA_ONLY_CLAUSE order by datname;"
@@ -86,13 +86,13 @@ echo -e "The following databases were matched for schema-only backup:\n${SCHEMA_
 
 for DATABASE in $SCHEMA_ONLY_DB_LIST
 do
-        echo "Schema-only backup of $DATABASE"
+  echo "Schema-only backup of $DATABASE"
 
-        if ! pg_dump -Fp -s -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz.in_progress; then
-                echo "[!!ERROR!!] Failed to backup database schema of $DATABASE" 1>&2
-        else
-                mv $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz
-        fi
+  if ! pg_dump -Fp -s -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz.in_progress; then
+    echo "[!!ERROR!!] Failed to backup database schema of $DATABASE" 1>&2
+  else
+    mv $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz
+  fi
 done
 
 
@@ -102,7 +102,7 @@ done
 
 for SCHEMA_ONLY_DB in ${SCHEMA_ONLY_LIST//,/ }
 do
-        EXCLUDE_SCHEMA_ONLY_CLAUSE="$EXCLUDE_SCHEMA_ONLY_CLAUSE and datname !~ '$SCHEMA_ONLY_DB'"
+  EXCLUDE_SCHEMA_ONLY_CLAUSE="$EXCLUDE_SCHEMA_ONLY_CLAUSE and datname !~ '$SCHEMA_ONLY_DB'"
 done
 
 FULL_BACKUP_QUERY="select datname from pg_database where not datistemplate and datallowconn $EXCLUDE_SCHEMA_ONLY_CLAUSE order by datname;"
@@ -112,27 +112,27 @@ echo -e "--------------------------------------------\n"
 
 for DATABASE in `psql -h "$HOSTNAME" -U "$USERNAME" -At -c "$FULL_BACKUP_QUERY" postgres`
 do
-        if [ $ENABLE_PLAIN_BACKUPS = "yes" ]
-        then
-                echo "Plain backup of $DATABASE"
+  if [ $ENABLE_PLAIN_BACKUPS = "yes" ]
+  then
+    echo "Plain backup of $DATABASE"
 
-                if ! pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress; then
-                        echo "[!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
-                else
-                        mv $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE".sql.gz
-                fi
-        fi
+    if ! pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress; then
+      echo "[!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
+    else
+      mv $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE".sql.gz
+    fi
+  fi
 
-        if [ $ENABLE_CUSTOM_BACKUPS = "yes" ]
-        then
-                echo "Custom backup of $DATABASE"
+  if [ $ENABLE_CUSTOM_BACKUPS = "yes" ]
+  then
+    echo "Custom backup of $DATABASE"
 
-                if ! pg_dump -Fc -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" -f $FINAL_BACKUP_DIR"$DATABASE".custom.in_progress; then
-                        echo "[!!ERROR!!] Failed to produce custom backup database $DATABASE" 1>&2
-                else
-                        mv $FINAL_BACKUP_DIR"$DATABASE".custom.in_progress $FINAL_BACKUP_DIR"$DATABASE".custom
-                fi
-        fi
+    if ! pg_dump -Fc -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" -f $FINAL_BACKUP_DIR"$DATABASE".custom.in_progress; then
+      echo "[!!ERROR!!] Failed to produce custom backup database $DATABASE" 1>&2
+    else
+      mv $FINAL_BACKUP_DIR"$DATABASE".custom.in_progress $FINAL_BACKUP_DIR"$DATABASE".custom
+    fi
+  fi
 
 done
 
