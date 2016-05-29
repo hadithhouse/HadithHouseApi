6f5cbb6fb494ff86ce4d44dca0b2270d09d3a9c6
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2016-05-29 23:07:02 +0100

    feat(textprocessing): Add reformat_text() method
    Like the reformat features in IDEs, this method is used to unify the
    formats of hadith texts. For example, multi-line text gets converted
    to single-line text; unnecessary whitespaces after opening brackets
    and before closing brackets get removed, and so on.

--- HadithHouseWebsite/hadiths/tests.py
+++ HadithHouseWebsite/hadiths/tests.py
@@ -6,11 +6,12 @@ from hadiths.models import Person
 class PersonTestCase(TestCase):
   def test_pre_save(self):
     p = Person()
-    p.display_name = 'رافِد'
-    p.full_name = 'رافِد خالِد'
-    p.brief_desc = 'مُبَرْمِج'
+    p.display_name = 'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+    p.full_name = 'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+    p.brief_desc ='إخْتِبار إزَاْلة عَلامات التَشْكيل'
     p.save()
 
-    self.assertEqual(p.simple_display_name, 'رافد')
-    self.assertEqual(p.simple_full_name, 'رافد خالد')
-    self.assertEqual(p.simple_brief_desc, 'مبرمج')
+    self.assertEqual('اختبار ازالة علامات التشكيل', p.simple_display_name, )
+    self.assertEqual('اختبار ازالة علامات التشكيل', p.simple_full_name, )
+    self.assertEqual('اختبار ازالة علامات التشكيل', p.simple_brief_desc, )
+
--- HadithHouseWebsite/textprocessing/arabic.py
+++ HadithHouseWebsite/textprocessing/arabic.py
@@ -7,13 +7,32 @@ def remove_arabic_diactrictics(input):
   return re.sub(u'[\u064B-\u0652]', '', input, flags=re.MULTILINE)
 
 
-def unify_alif_letters(input):
+def unify_alef_letters(input):
   if input is None:
     return None
-  return re.sub(u'[\u0622\u0623\u0625]', u'\u0627', input, flags=re.MULTILINE)
+  # Source: "Arabic script in Unicode" at Wikipedia
+  alef = '\u0627'
+  alef_with_madda = '\u0622'
+  alef_with_hamza = '\u0623'
+  alef_with_hamza_below = '\u0625'
+  alef_wasla = '\u0671'
+  alef_with_wavy_hamza = '\u0672'
+  alef_with_wavy_hamza_below = '\u0673'
+  alef_high_hamza_alef = '\u0675'
+  regex = '[' + ''.join([
+    alef_with_madda,
+    alef_with_hamza,
+    alef_with_hamza_below,
+    alef_wasla,
+    alef_with_wavy_hamza,
+    alef_with_wavy_hamza_below,
+    alef_high_hamza_alef
+  ]) + ']'
+  return re.sub(regex, u'\u0627', input, flags=re.MULTILINE)
 
 
 def simplify_arabic_text(input):
   if input is None:
     return None
-  return unify_alif_letters(remove_arabic_diactrictics(input))
+  return unify_alef_letters(remove_arabic_diactrictics(input))
+
