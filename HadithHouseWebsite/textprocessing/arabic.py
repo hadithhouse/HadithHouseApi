d8677cdfa1ece854d6577665bd0ea0117f3e2043
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2016-05-31 13:10:41 +0100

    feat(textprocessing): Implement DocScanner class
    This class used to find certain tokens in a given document. The
    tokens can be specified by regular expressions. This is going to be
    used to scan books and extract hadiths from them.
    
    #183

--- HadithHouseWebsite/textprocessing/arabic.py
+++ HadithHouseWebsite/textprocessing/arabic.py
@@ -1,7 +1,7 @@
 import re
 
 
-def remove_arabic_diactrictics(input):
+def remove_arabic_diacritics(input):
   if input is None:
     return None
   return re.sub(u'[\u064B-\u0652]', '', input, flags=re.MULTILINE)
@@ -28,11 +28,11 @@ def unify_alef_letters(input):
     alef_with_wavy_hamza_below,
     alef_high_hamza_alef
   ]) + ']'
-  return re.sub(regex, u'\u0627', input, flags=re.MULTILINE)
+  return re.sub(regex, alef, input, flags=re.MULTILINE)
 
 
 def simplify_arabic_text(input):
   if input is None:
     return None
-  return unify_alef_letters(remove_arabic_diactrictics(input))
+  return unify_alef_letters(remove_arabic_diacritics(input))
 
