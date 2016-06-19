d977c8ae59d5137d617f29c4de754b6958a1dd7d
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2016-06-19 18:42:18 +0100

    feat(jsuglify): Support compressing JS files during build
    #94

--- HadithHouseWebsite/Gruntfile.js
+++ HadithHouseWebsite/Gruntfile.js
@@ -1,11 +1,52 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        ts: {
-            default : {
-                src: ["hadiths/static/hadiths/js/**/*.ts"]
-            }
-        }
-    });
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.registerTask("default", ["ts"]);
+  grunt.initConfig({
+    ts: {
+      default : {
+        src: ['hadiths/static/hadiths/js/**/*.ts']
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false,
+        preserveComments: false,
+        sourceMap: true,
+        sourceMapIncludeSources: true,
+        compress: false
+      },
+      all: {
+        files: [
+          {
+            // concat.dist.dest refers to the above concatenation destination
+            'hadiths/static/hadiths/js/all.js': [
+              'hadiths/static/hadiths/js/app.js',
+              'hadiths/static/hadiths/js/services/facebook.service.js',
+              'hadiths/static/hadiths/js/services/services.js',
+              'hadiths/static/hadiths/js/services/toast.service.js',
+              'hadiths/static/hadiths/js/caching/cache.js',
+              'hadiths/static/hadiths/js/resources/resources.js',
+              'hadiths/static/hadiths/js/controllers/entity-page.js',
+              'hadiths/static/hadiths/js/controllers/entity-listing-page.js',
+              'hadiths/static/hadiths/js/controllers/book-page.js',
+              'hadiths/static/hadiths/js/controllers/book-listing-page.js',
+              'hadiths/static/hadiths/js/controllers/hadith-page.js',
+              'hadiths/static/hadiths/js/controllers/hadith-listing-page.js',
+              'hadiths/static/hadiths/js/controllers/hadithtag-page.js',
+              'hadiths/static/hadiths/js/controllers/hadithtag-listing-page.js',
+              'hadiths/static/hadiths/js/controllers/home-page.js',
+              'hadiths/static/hadiths/js/controllers/person-page.js',
+              'hadiths/static/hadiths/js/controllers/person-listing-page.js',
+              'hadiths/static/hadiths/js/controllers/user-page.js',
+              'hadiths/static/hadiths/js/controllers/user-listing-page.js',
+              'hadiths/static/hadiths/js/directives/selector.directive.js',
+              'hadiths/static/hadiths/js/directives/tree.directive.js'
+            ]
+          }
+        ]
+      },
+    }
+  });
+  grunt.loadNpmTasks('grunt-ts');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['ts', 'uglify']);
 };
