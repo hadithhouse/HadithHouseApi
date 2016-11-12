e6ec13c9b6844993cab0ca261fd11607afbed5da
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2016-11-12 10:53:46 +0000

    fix(test) Removing Python 3 specific code
    This broke the build since it is using Python 2.
    
    Issue #245

--- HadithHouseWebsite/HadithHouseWebsite/exception_handler.py
+++ HadithHouseWebsite/HadithHouseWebsite/exception_handler.py
@@ -1,8 +1,7 @@
-from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
 
-def hadithhouse_exception_handler(ex: Exception, context) -> Response:
+def hadithhouse_exception_handler(ex, context):
   response = exception_handler(ex, context)
   if response is not None:
     response.data['status_code'] = response.status_code
