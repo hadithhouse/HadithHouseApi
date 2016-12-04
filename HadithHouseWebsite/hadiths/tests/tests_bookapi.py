25f3661be75c5f4c218ff0e965f80866467fc302
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2016-12-04 11:48:11 +0000

    test(apis) Add tests for PUT APIs
    - Book API
    - Hadith API
    - Person API
    
    Issue #245

--- HadithHouseWebsite/hadiths/tests/tests_bookapi.py
+++ HadithHouseWebsite/hadiths/tests/tests_bookapi.py
@@ -1,9 +1,10 @@
+from django.test import Client
 from rest_framework.status import HTTP_403_FORBIDDEN, HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_201_CREATED
 
 from hadiths.tests.setup import TestCaseBase
 
 
-class BookApiTestCase(TestCaseBase):
+class BookPostApiTestCase(TestCaseBase):
   def test__post__no_auth_token__403(self):
     resp = self.post('/apis/books', {'title': 'test'})
     self.assertEqual(HTTP_403_FORBIDDEN, resp.status_code)
@@ -51,6 +52,68 @@ class BookApiTestCase(TestCaseBase):
     self.assertEqual(HTTP_201_CREATED, resp.status_code)
     book = resp.data
     self.assertEqual('test', book['title'])
+
+    resp2 = self.get('/apis/books/%d' % book['id'])
+    self.assertEqual(HTTP_200_OK, resp2.status_code)
+    book2 = resp2.data
+    self.assertEqual(book, book2)
+
+
+class BookPutApiTestCase(TestCaseBase):
+  book = None
+  book_id = None
+
+  @classmethod
+  def setUpClass(cls):
+    c = Client()
+    resp = c.post('/apis/books?fb_token=%s' % TestCaseBase.marie_accesstoken, {'title': 'test'})
+    assert resp.status_code == HTTP_201_CREATED
+    cls.book = resp.data
+    cls.book_id = cls.book['id']
+
+  def test__put__no_auth_token__403(self):
+    resp = self.put('/apis/books/%d' % BookPutApiTestCase.book_id, {'title': 'test'})
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.status_code)
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.data['status_code'])
+    self.assertEqual("Couldn't authenticate user.", resp.data['error'])
+
+  def test__put__invalid_auth_token__403(self):
+    resp = self.put('/apis/books/%d?fb_token=%s' % (BookPutApiTestCase.book_id, TestCaseBase.invalid_accesstoken),
+                    {'title': 'test'})
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.status_code)
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.data['status_code'])
+    self.assertEqual("Invalid Facebook access token.", resp.data['error'])
+
+  def test__put__valid_auth_token__no_user_permission__401(self):
+    resp = self.put('/apis/books/%d?fb_token=%s' % (BookPutApiTestCase.book_id, TestCaseBase.jack_accesstoken),
+                    {'title': 'test'})
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.status_code)
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.data['status_code'])
+    self.assertEqual("User doesn't have permission for this action.", resp.data['error'])
+
+  def test__put__valid_auth_token__user_permission__no_title__400(self):
+    resp = self.put('/apis/books/%d?fb_token=%s' % (BookPutApiTestCase.book_id, TestCaseBase.marie_accesstoken), {})
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.status_code)
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.data['status_code'])
+    self.assertEqual("Invalid input.", resp.data['error'])
+    self.assertTrue('title' in resp.data['detail'])
+    self.assertEqual(['This field is required.'], resp.data['detail']['title'])
+
+  def test__put__valid_auth_token__user_permission__blank_title__400(self):
+    resp = self.put('/apis/books/%d?fb_token=%s' % (BookPutApiTestCase.book_id, TestCaseBase.marie_accesstoken),
+                    {'title': ' '})
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.status_code)
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.data['status_code'])
+    self.assertEqual("Invalid input.", resp.data['error'])
+    self.assertTrue('title' in resp.data['detail'])
+    self.assertEqual(['This field may not be blank.'], resp.data['detail']['title'])
+
+  def test__put__valid_auth_token__user_permission__valid_title__book_updated(self):
+    resp = self.put('/apis/books/%d?fb_token=%s' % (BookPutApiTestCase.book_id, TestCaseBase.marie_accesstoken),
+                    {'title': 'test_updated'})
+    self.assertEqual(HTTP_200_OK, resp.status_code)
+    book = resp.data
+    self.assertEqual('test_updated', book['title'])
 
     resp2 = self.get('/apis/books/%d' % book['id'])
     self.assertEqual(HTTP_200_OK, resp2.status_code)
--- HadithHouseWebsite/hadiths/tests/tests_personapi.py
+++ HadithHouseWebsite/hadiths/tests/tests_personapi.py
@@ -1,3 +1,4 @@
+from django.test import Client
 from rest_framework.status import HTTP_403_FORBIDDEN, HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_201_CREATED
 
 from hadiths.tests.setup import TestCaseBase
@@ -51,6 +52,72 @@ class PersonApiTestCase(TestCaseBase):
     self.assertEqual(HTTP_201_CREATED, resp.status_code)
     person = resp.data
     self.assertEqual('test', person['full_name'])
+
+    resp2 = self.get('/apis/persons/%d' % person['id'])
+    self.assertEqual(HTTP_200_OK, resp2.status_code)
+    person2 = resp2.data
+    self.assertEqual(person, person2)
+
+
+class PersonPutApiTestCase(TestCaseBase):
+  person = None
+  person_id = None
+
+  @classmethod
+  def setUpClass(cls):
+    c = Client()
+    resp = c.post('/apis/persons?fb_token=%s' % TestCaseBase.marie_accesstoken, {'full_name': 'test'})
+    assert resp.status_code == HTTP_201_CREATED
+    cls.person = resp.data
+    cls.person_id = cls.person['id']
+
+  def test__no_auth_token__403(self):
+    resp = self.put('/apis/persons/%d' % PersonPutApiTestCase.person_id, {'full_name': 'test'})
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.status_code)
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.data['status_code'])
+    self.assertEqual("Couldn't authenticate user.", resp.data['error'])
+
+  def test__invalid_auth_token__403(self):
+    resp = self.put(
+      '/apis/persons/%d?fb_token=%s' % (PersonPutApiTestCase.person_id, TestCaseBase.invalid_accesstoken),
+      {'full_name': 'test'})
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.status_code)
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.data['status_code'])
+    self.assertEqual("Invalid Facebook access token.", resp.data['error'])
+
+  def test__valid_auth_token__no_user_permission__401(self):
+    resp = self.put('/apis/persons/%d?fb_token=%s' % (PersonPutApiTestCase.person_id, TestCaseBase.jack_accesstoken),
+                    {'full_name': 'test'})
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.status_code)
+    self.assertEqual(HTTP_403_FORBIDDEN, resp.data['status_code'])
+    self.assertEqual("User doesn't have permission for this action.", resp.data['error'])
+
+  def test__valid_auth_token__user_permission__no_name__400(self):
+    resp = self.put(
+      '/apis/persons/%d?fb_token=%s' % (PersonPutApiTestCase.person_id, TestCaseBase.marie_accesstoken), {})
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.status_code)
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.data['status_code'])
+    self.assertEqual("Invalid input.", resp.data['error'])
+    self.assertTrue('full_name' in resp.data['detail'])
+    self.assertEqual(['This field is required.'], resp.data['detail']['full_name'])
+
+  def test__valid_auth_token__user_permission__blank_name__400(self):
+    resp = self.put(
+      '/apis/persons/%d?fb_token=%s' % (PersonPutApiTestCase.person_id, TestCaseBase.marie_accesstoken),
+      {'full_name': ' '})
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.status_code)
+    self.assertEqual(HTTP_400_BAD_REQUEST, resp.data['status_code'])
+    self.assertEqual("Invalid input.", resp.data['error'])
+    self.assertTrue('full_name' in resp.data['detail'])
+    self.assertEqual(['This field may not be blank.'], resp.data['detail']['full_name'])
+
+  def test__valid_auth_token__user_permission__valid_new_name__person_updated(self):
+    resp = self.put(
+      '/apis/persons/%d?fb_token=%s' % (PersonPutApiTestCase.person_id, TestCaseBase.marie_accesstoken),
+      {'full_name': 'test_updated'})
+    self.assertEqual(HTTP_200_OK, resp.status_code)
+    person = resp.data
+    self.assertEqual('test_updated', person['full_name'])
 
     resp2 = self.get('/apis/persons/%d' % person['id'])
     self.assertEqual(HTTP_200_OK, resp2.status_code)
