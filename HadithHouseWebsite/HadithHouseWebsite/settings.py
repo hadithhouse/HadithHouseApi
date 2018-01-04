bf0a003a05e3b9b300a4642cd9b8d162f7987ae7
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-01-03 22:04:26 -0800

    chore(python) PEP 8 styling
    Issue #342

--- HadithHouseWebsite/HadithHouseWebsite/settings.py
+++ HadithHouseWebsite/HadithHouseWebsite/settings.py
@@ -1,3 +1,27 @@
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Rafid Khalid Al-Humaimidi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
 """
 Django settings for HadithHouseWebsite project.
 
@@ -22,38 +46,44 @@ OFFLINE_MODE = False
 
 
 def is_test_mode():
-  """
-  Determines whether the applicaiton is being currently running in test mode,
-  i.e. python manage.py test.
-  :return: True or false.
-  """
-  return test_mode
+    """
+    Determines whether the applicaiton is being currently running in test mode,
+    i.e. python manage.py test.
+    :return: True or false.
+    """
+    return test_mode
 
 
 def is_offline_mode():
-  return OFFLINE_MODE
+    """
+    Determines whether the application is running in offline mode. In this mode,
+    the website uses locally fetched JS libraries and uses a sqlite local
+    database.
+    :return: True or False.
+    """
+    return OFFLINE_MODE
 
 
 def is_collectstatic_mode():
-  """
-  Determines whether the code is being executed during a call to
-  "python manage.py collectstatic".
-  :return: True or false.
-  """
-  return collectstatic_mode
+    """
+    Determines whether the code is being executed during a call to
+    "python manage.py collectstatic".
+    :return: True or false.
+    """
+    return collectstatic_mode
 
 
 if is_test_mode() or is_collectstatic_mode():
-  # We are running in test mode or collecting static files. Hence, avoid using
-  # the real log directory to avoid breaking Jenkins build, as there is no
-  # log directory on Jenkins.
-  import tempfile
+    # We are running in test mode or collecting static files. Hence, avoid using
+    # the real log directory to avoid breaking Jenkins build, as there is no
+    # log directory on Jenkins.
+    import tempfile
 
 
-  def get_log_dir():
-    return tempfile.gettempdir()
+    def get_log_dir():
+        return tempfile.gettempdir()
 else:
-  from HadithHouseWebsite.server_settings import get_log_dir
+    from HadithHouseWebsite.server_settings import get_log_dir
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -70,23 +100,23 @@ DJANGO_LOG_LEVEL = DEBUG
 ALLOWED_HOSTS = get_allowed_hosts()
 
 PRODUCTION_HOSTS = (
-  'www.hadithhouse.net',
+    'www.hadithhouse.net',
 )
 
 DEVELOPMENT_HOSTS = (
-  'dev.hadithhouse.net',
+    'dev.hadithhouse.net',
 )
 
 
 def get_environment():
-  host = socket.getfqdn().lower()
+    host = socket.getfqdn().lower()
 
-  if host in PRODUCTION_HOSTS:
-    return 'production'
-  elif host in DEVELOPMENT_HOSTS:
-    return 'development'
-  else:
-    return 'local'
+    if host in PRODUCTION_HOSTS:
+        return 'production'
+    elif host in DEVELOPMENT_HOSTS:
+        return 'development'
+    else:
+        return 'local'
 
 
 SERVER_EMAIL = 'noreply@hadithhouse.net'
@@ -94,24 +124,24 @@ SERVER_EMAIL = 'noreply@hadithhouse.net'
 # Application definition
 
 INSTALLED_APPS = (
-  'django.contrib.admin',
-  'django.contrib.auth',
-  'django.contrib.contenttypes',
-  'django.contrib.sessions',
-  'django.contrib.messages',
-  'django.contrib.staticfiles',
-  'rest_framework',
-  'hadiths',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'hadiths',
 )
 
 MIDDLEWARE_CLASSES = (
-  'django.contrib.sessions.middleware.SessionMiddleware',
-  'django.middleware.common.CommonMiddleware',
-  'django.middleware.csrf.CsrfViewMiddleware',
-  'django.contrib.auth.middleware.AuthenticationMiddleware',
-  'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-  'django.contrib.messages.middleware.MessageMiddleware',
-  'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'HadithHouseWebsite.urls'
@@ -121,28 +151,28 @@ APPEND_SLASH = False
 WSGI_APPLICATION = 'HadithHouseWebsite.wsgi.application'
 
 ADMINS = (
-  ('Rafid Al-Humaimidi', 'admin@hadithhouse.net'),
+    ('Rafid Al-Humaimidi', 'admin@hadithhouse.net'),
 )
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 # This
 if is_test_mode():
-  DATABASES = {
-    'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': os.path.join(BASE_DIR, 'HadithHouse-Test.db'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'HadithHouse-Test.db'),
+        }
     }
-  }
 elif is_offline_mode():
-  DATABASES = {
-    'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': os.path.join(BASE_DIR, 'HadithHouse-OfflineMode.db'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'HadithHouse-OfflineMode.db'),
+        }
     }
-  }
 else:
-  DATABASES = get_db_settings()
+    DATABASES = get_db_settings()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -166,118 +196,118 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-  # Put strings here, like "/home/html/static" or "C:/www/django/static".
-  # Always use forward slashes, even on Windows.
-  # Don't forget to use absolute paths, not relative paths.
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 TEMPLATES = [
-  {
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [
-      # insert your TEMPLATE_DIRS here
-      os.path.join(BASE_DIR, 'templates'),
-    ],
-    'APP_DIRS': True,
-    'OPTIONS': {
-      'context_processors': [
-        # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-        # list if you haven't customized them:
-        'django.contrib.auth.context_processors.auth',
-        'django.template.context_processors.debug',
-        'django.template.context_processors.i18n',
-        'django.template.context_processors.media',
-        'django.template.context_processors.static',
-        'django.template.context_processors.tz',
-        'django.contrib.messages.context_processors.messages',
-      ],
-      'debug': get_debug()
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': get_debug()
+        },
     },
-  },
 ]
 
 LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'formatters': {
-    'verbose': {
-      'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
+        },
     },
-    'simple': {
-      'format': '%(levelname)s %(asctime)s %(message)s'
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
     },
-  },
-  'filters': {
-    'require_debug_true': {
-      '()': 'django.utils.log.RequireDebugTrue',
+    'handlers': {
+        'django_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(get_log_dir(), 'django.log'),
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 30,
+            'utc': True,
+            'formatter': 'simple',
+            'encoding': 'utf-8'
+        },
+        'django_requests_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(get_log_dir(), 'django.request.log'),
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 30,
+            'utc': True,
+            'formatter': 'simple',
+            'encoding': 'utf-8'
+        },
+        'django_db_backends_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(get_log_dir(), 'django.db.backends.log'),
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 30,
+            'utc': True,
+            'formatter': 'simple',
+            'encoding': 'utf-8'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
     },
-  },
-  'handlers': {
-    'django_log_file': {
-      'level': 'DEBUG',
-      'class': 'logging.handlers.TimedRotatingFileHandler',
-      'filename': os.path.join(get_log_dir(), 'django.log'),
-      'when': 'D',
-      'interval': 1,
-      'backupCount': 30,
-      'utc': True,
-      'formatter': 'simple',
-      'encoding': 'utf-8'
-    },
-    'django_requests_log_file': {
-      'level': 'DEBUG',
-      'class': 'logging.handlers.TimedRotatingFileHandler',
-      'filename': os.path.join(get_log_dir(), 'django.request.log'),
-      'when': 'D',
-      'interval': 1,
-      'backupCount': 30,
-      'utc': True,
-      'formatter': 'simple',
-      'encoding': 'utf-8'
-    },
-    'django_db_backends_log_file': {
-      'level': 'DEBUG',
-      'class': 'logging.handlers.TimedRotatingFileHandler',
-      'filename': os.path.join(get_log_dir(), 'django.db.backends.log'),
-      'when': 'D',
-      'interval': 1,
-      'backupCount': 30,
-      'utc': True,
-      'formatter': 'simple',
-      'encoding': 'utf-8'
-    },
-    'mail_admins': {
-      'level': 'ERROR',
-      'class': 'django.utils.log.AdminEmailHandler'
+    'loggers': {
+        'django': {
+            'handlers': ['django_log_file'],
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['django_requests_log_file', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['django_db_backends_log_file'],
+            'propagate': False,
+        },
     }
-  },
-  'loggers': {
-    'django': {
-      'handlers': ['django_log_file'],
-      'propagate': True,
-    },
-    'django.request': {
-      'handlers': ['django_requests_log_file', 'mail_admins'],
-      'level': 'ERROR',
-      'propagate': False,
-    },
-    'django.db.backends': {
-      'level': 'DEBUG',
-      'handlers': ['django_db_backends_log_file'],
-      'propagate': False,
-    },
-  }
 }
 
 REST_FRAMEWORK = {
-  'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
-  'DEFAULT_PAGINATION_CLASS': 'hadiths.pagination.DefaultPagination',
-  'PAGE_SIZE': 10,
-  'DEFAULT_AUTHENTICATION_CLASSES': (
-    'hadiths.auth.FacebookAuthentication',
-  ),
-  'EXCEPTION_HANDLER': 'HadithHouseWebsite.exception_handler.hadithhouse_exception_handler'
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'hadiths.pagination.DefaultPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'hadiths.auth.FacebookAuthentication',
+    ),
+    'EXCEPTION_HANDLER': 'HadithHouseWebsite.exception_handler.hadithhouse_exception_handler'
 }
 
 if OFFLINE_MODE:
-  REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = ('hadiths.auth.FacebookOfflineAuthentication',)
+    REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = ('hadiths.auth.FacebookOfflineAuthentication',)
--- HadithHouseWebsite/hadiths/views.py
+++ HadithHouseWebsite/hadiths/views.py
@@ -1,3 +1,27 @@
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Rafid Khalid Al-Humaimidi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.staticfiles import finders
@@ -11,51 +35,51 @@ all_css_hash = None
 
 
 def md5(file_name):
-  """
-  Find the MD5 hash of the given file.
-  Taken from: http://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+    """
+    Find the MD5 hash of the given file.
+    Taken from: http://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
 
-  :param file_name: The name of the file
-  :return: The MD5 hash
-  """
-  hash_md5 = hashlib.md5()
-  with open(file_name, "rb") as f:
-    for chunk in iter(lambda: f.read(4096), b""):
-      hash_md5.update(chunk)
-  return hash_md5.hexdigest()
+    :param file_name: The name of the file
+    :return: The MD5 hash
+    """
+    hash_md5 = hashlib.md5()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 
 def set_all_js_hash():
-  # Find the MD5 hash of all.js. This is used in index.html for cache busting.
-  # TODO: Perhaps consider saving it to file to avoid re-calculating it every time the
-  # application is restarted?
-  # https://github.com/hadithhouse/hadithhouse/issues/307
-  global all_js_hash
-  if all_js_hash is None and settings.get_environment() == 'production':
-    all_js_hash = md5(finders.find('hadiths/js/all.js'))
+    # Find the MD5 hash of all.js. This is used in index.html for cache busting.
+    # TODO: Perhaps consider saving it to file to avoid re-calculating it every time the
+    # application is restarted?
+    # https://github.com/hadithhouse/hadithhouse/issues/307
+    global all_js_hash
+    if all_js_hash is None and settings.get_environment() == 'production':
+        all_js_hash = md5(finders.find('hadiths/js/all.js'))
 
 
 def set_all_css_hash():
-  # Find the MD5 hash of all.css. This is used in index.html for cache busting.
-  # TODO: Perhaps consider saving it to file to avoid re-calculating it every time the
-  # application is restarted?
-  # https://github.com/hadithhouse/hadithhouse/issues/307
-  global all_css_hash
-  if all_css_hash is None and settings.get_environment() == 'production':
-    all_css_hash = md5(finders.find('hadiths/css/all.css'))
+    # Find the MD5 hash of all.css. This is used in index.html for cache busting.
+    # TODO: Perhaps consider saving it to file to avoid re-calculating it every time the
+    # application is restarted?
+    # https://github.com/hadithhouse/hadithhouse/issues/307
+    global all_css_hash
+    if all_css_hash is None and settings.get_environment() == 'production':
+        all_css_hash = md5(finders.find('hadiths/css/all.css'))
 
 
 def index(request, path):
-  set_all_js_hash()
-  set_all_css_hash()
+    set_all_js_hash()
+    set_all_css_hash()
 
-  context = {
-    'appId': get_fb_appid(),
-    'offline_mode': settings.OFFLINE_MODE,
-    'environment': settings.get_environment(),
-    'all_js_hash': all_js_hash,
-    'all_css_hash': all_css_hash,
-  }
+    context = {
+        'appId': get_fb_appid(),
+        'offline_mode': settings.OFFLINE_MODE,
+        'environment': settings.get_environment(),
+        'all_js_hash': all_js_hash,
+        'all_css_hash': all_css_hash,
+    }
 
-  template = loader.get_template('hadiths/index.html')
-  return HttpResponse(template.render(context))
+    template = loader.get_template('hadiths/index.html')
+    return HttpResponse(template.render(context))
