ee778c373d33b416cc89985a36c45d2804448ad2
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-01-06 23:43:56 -0800

    feat(data) Refactoring textprocessing/ and importdata.py
    Refactoring according to PEP-8 4-space, 80-space margin style.
    
    Issue #342

--- HadithHouseWebsite/textprocessing/regex.py
+++ HadithHouseWebsite/textprocessing/regex.py
@@ -2,36 +2,46 @@ import re
 
 
 class DocScanner(object):
-  """
-  A class used to find certain tokens in a given document. The tokens can be
-  specified by regular expressions.
-  """
-
-  def __init__(self, tokens_dict, callback):
     """
-    Initialize a new document scanner.
-    :param tokens_dict: A dictionary whose keys are the types of tokens and
-    values are the regex for finding such types of tokens.
-    :param callback: A function to be called whenever a token is found.
+    A class used to find certain tokens in a given document. The tokens can be
+    specified by regular expressions.
     """
-    self.types = list(tokens_dict.keys())
-    self.scanning_regex = '|'.join(['(?P<%s>%s)' % (type, regex) for type, regex in tokens_dict.items()])
-    self.callback = callback
 
-  def scan(self, document, context=None):
-    prev_match = None
-    prev_type = None
-    for match in re.finditer(self.scanning_regex, document, flags=re.MULTILINE):
-      for type in self.types:
-        if match.group(type) is not None:
-          self.callback(
-            type,
-            prev_type,
-            match,
-            prev_match,
-            document,
-            context
-          )
-          prev_type = type
-          prev_match = match
-          break
+    def __init__(self, tokens_dict, callback):
+        """
+        Initialize a new document scanner.
+        :param tokens_dict: A dictionary whose keys are the types of tokens and
+        values are the regex for finding such types of tokens.
+        :param callback: A function to be called whenever a token is found.
+        """
+        self.types = list(tokens_dict.keys())
+        self.scanning_regex = '|'.join(
+            ['(?P<%s>%s)' % (type, regex) for type, regex in
+             tokens_dict.items()])
+        self.callback = callback
+
+    def scan(self, document, context=None):
+        """
+        Starts the scanning process.
+        :param document: The document to start the scanning process on.
+        :param context: A context to be sent to the call back. The caller is
+        pass any thing to this parameter.
+        :return:
+        """
+        prev_match = None
+        prev_type = None
+        for match in re.finditer(self.scanning_regex, document,
+                                 flags=re.MULTILINE):
+            for type in self.types:
+                if match.group(type) is not None:
+                    self.callback(
+                        type,
+                        prev_type,
+                        match,
+                        prev_match,
+                        document,
+                        context
+                    )
+                    prev_type = type
+                    prev_match = match
+                    break
