8a46d3c821985907d6788fb74a9a201054c07023
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-01-07 21:55:01 -0800

    feat(data) Progress towards importing Al-Kafi hadiths
    The scripts almost work and tried on the first three volumes. It does seem to be working, though I actually only skimmed the generated JSON instead of going through the hadiths one by one for obvious reasons.
    
    One problem I still need to address is correctly matching kitabs and babs to chapters and sections, respectively. Right now, the 'bab' is mapped to chapter and nothing is mapped to section.
    
    Issue #83

--- HadithHouseWebsite/hadiths/management/commands/importdata.py
+++ HadithHouseWebsite/hadiths/management/commands/importdata.py
@@ -1,25 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import codecs
-import os
-import re
-
 from django.core.management.base import BaseCommand, CommandError
 
-from HadithHouseWebsite import settings
+from hadiths.management.commands.alkafi import import_alkafi_volume
 from hadiths.management.commands.holyquran import import_holyquran
-from hadiths.models import Hadith, Book, BookVolume, BookChapter
-from hadiths import initial_data
-from textprocessing.generic import reformat_text
-from textprocessing.regex import DocScanner
 
 
 class Command(BaseCommand):
     help = 'Import data to the website'
-
-    @staticmethod
-    def get_book_path(sub_path):
-        return os.path.join(os.path.join(settings.BASE_DIR, 'books'), sub_path)
 
     def add_arguments(self, parser):
         parser.add_argument('dataname', nargs=1, type=str)
@@ -29,69 +17,10 @@ class Command(BaseCommand):
         if data_name == 'holyquran':
             import_holyquran(self)
         elif data_name == 'alkafi-v1':
-            self.import_alkafi_v1()
+            import_alkafi_volume(self, 1)
+        elif data_name == 'alkafi-v2':
+            import_alkafi_volume(self, 2)
+        elif data_name == 'alkafi-v3':
+            import_alkafi_volume(self, 3)
         else:
             raise CommandError('Invalid data name specified: ' + data_name)
-
-    def import_alkafi_v1(self):
-        input_path = Command.get_book_path('al-kafi/vol1/alkafi_1.txt')
-
-        # Open the input file and remove some unnecessary parts in it.
-        with codecs.open(input_path, 'r', 'utf-8') as input_file:
-            content = input_file.read()
-        part_of_line_to_remove = 'الكافي : المجلد الأول'
-        part_of_line_to_remove_regex = '^' + part_of_line_to_remove + '.*$'
-        content = re.sub(part_of_line_to_remove_regex, '', content,
-                         flags=re.MULTILINE)
-
-        hadith_info = {
-            'kitab': None,
-            'bab': None,
-        }
-
-        alkafi_v1_title = u"الكافي - الجزء الأول"
-        alkafi = Book.objects.get(title=initial_data.shia_first_hadith_book)
-        alkafi_v1, volume_created = BookVolume.objects.get_or_create(
-            title=alkafi_v1_title, book=alkafi, number=1)
-        chapter = [None]
-        chapter_no = [1]
-
-        def callback(type, prev_type, match, prev_match, doc, context):
-            if type == 'kitab':
-                ignore = 'كِتَابُ هِشَامِ بْنِ عَبْدِ الْمَلِكِ'
-                kitab = reformat_text(doc[match.start():match.end()])
-                if not kitab.startswith(ignore):
-                    hadith_info['kitab'] = kitab
-            elif type == 'bab':
-                hadith_info['bab'] = reformat_text(
-                    doc[match.start():match.end()])
-                chapter[0], chapter_created = BookChapter.objects.get_or_create(
-                    book=alkafi,
-                    volume=alkafi_v1,
-                    title=alkafi_v1_title,
-                    number=chapter_no[0])
-                chapter_no[0] += 1
-            if prev_type == 'hadith_num':
-                hadith_info['hadith'] = reformat_text(
-                    doc[prev_match.end():match.start()])
-                hadith_info['num'] = int(
-                    doc[prev_match.start():prev_match.end() - 1])
-
-                Hadith.objects.get_or_create(
-                    text=hadith_info['hadith'],
-                    book=alkafi,
-                    volume=alkafi_v1,
-                    chapter=chapter[0],
-                    number=hadith_info['num'])
-
-        kitab_word_regex = '\u0643\u0650?\u062A\u064E?\u0627\u0628?\u064F'
-        bab_word_regex = '\u0628\u064e?\u0627\u0628\u064f?'
-        ds = DocScanner({
-            'kitab': '^\s*' + kitab_word_regex + '.*',
-            'bab': '^\s*' + bab_word_regex + '.*',
-            'hadith_num': '^\s*[\u0660-\u06690-9]+\s*-',
-            'eof': '\Z'
-        }, callback)
-        ds.scan(content, {})
-
-        print('Finished processing input file.')
