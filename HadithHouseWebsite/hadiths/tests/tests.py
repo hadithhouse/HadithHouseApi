ee112c0b7200ee77f30708a87debf9db50c91248
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-01-14 19:49:07 -0800

    chore(python) Refactoring to match PEP8 style
    Issue #342

--- HadithHouseWebsite/hadiths/tests/tests.py
+++ HadithHouseWebsite/hadiths/tests/tests.py
@@ -1,77 +1,78 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 
-from hadiths.models import Person, Book, BookVolume, BookChapter, BookSection, HadithTag, Hadith
+from hadiths.models import Person, Book, BookVolume, BookChapter, BookSection, \
+    HadithTag, Hadith
 
 
 class PersonTestCase(TestCase):
-  def test_pre_save(self):
-    p = Person()
-    p.display_name = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    p.full_name = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    p.brief_desc = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    p.save()
+    def test_pre_save(self):
+        p = Person()
+        p.display_name = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        p.full_name = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        p.brief_desc = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        p.save()
 
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_display_name)
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_full_name)
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_brief_desc)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_display_name)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_full_name)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_brief_desc)
 
 
 class BookTestCase(TestCase):
-  def test_pre_save(self):
-    b = Book()
-    b.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    b.brief_desc = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    b.save()
+    def test_pre_save(self):
+        b = Book()
+        b.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        b.brief_desc = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        b.save()
 
-    bv = BookVolume()
-    bv.number = 1
-    bv.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    bv.book = b
-    bv.save()
+        bv = BookVolume()
+        bv.number = 1
+        bv.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        bv.book = b
+        bv.save()
 
-    bc = BookChapter()
-    bc.number = 1
-    bc.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    bc.book = b
-    bc.save()
+        bc = BookChapter()
+        bc.number = 1
+        bc.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        bc.book = b
+        bc.save()
 
-    bs = BookSection()
-    bs.number = 1
-    bs.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    bs.book = b
-    bs.save()
+        bs = BookSection()
+        bs.number = 1
+        bs.title = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        bs.book = b
+        bs.save()
 
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', b.simple_title)
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', b.simple_brief_desc)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', b.simple_title)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', b.simple_brief_desc)
 
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', bv.simple_title)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', bv.simple_title)
 
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', bc.simple_title)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', bc.simple_title)
 
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', bs.simple_title)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', bs.simple_title)
 
 
 class HadithTagTestCase(TestCase):
-  def test_pre_save(self):
-    p = HadithTag()
-    p.name = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    p.save()
+    def test_pre_save(self):
+        p = HadithTag()
+        p.name = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        p.save()
 
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_name)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_name)
 
 
 class HadithTestCase(TestCase):
-  def test_pre_save(self):
-    p = Hadith()
-    p.text = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
-    p.save()
+    def test_pre_save(self):
+        p = Hadith()
+        p.text = u'إخْتِبار إزَاْلة عَلامات التَشْكيل'
+        p.save()
 
-    self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_text)
+        self.assertEqual(u'اختبار ازالة علامات التشكيل', p.simple_text)
 
 
 class TestModeTestCase(TestCase):
-  def test__is_test_mode__returns_true(self):
-    from HadithHouseWebsite.settings import is_test_mode
-    self.assertEqual(True, is_test_mode(), "is_test_mode() should return True.")
-
+    def test__is_test_mode__returns_true(self):
+        from HadithHouseWebsite.settings import is_test_mode
+        self.assertEqual(True, is_test_mode(),
+                         "is_test_mode() should return True.")
