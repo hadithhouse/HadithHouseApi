f8f76fccfadbd51144645b3465af42f79a67fefb
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-02-11 22:29:51 -0800

    feat(ui-rewrite) Fix tslint error

--- hadiths/static/hadiths/js-angular/main.ts
+++ hadiths/static/hadiths/js-angular/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "app.module";
 
-//enableProdMode();
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
