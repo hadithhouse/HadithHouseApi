2e5f1ced61aa054f3cc778d6f21656ca42cd8a9d
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-02-11 22:19:53 -0800

    feat(ui-rewrite) Angulal hello world
    Issue #387

--- hadiths/static/hadiths/js-angular/app.component.ts
+++ hadiths/static/hadiths/js-angular/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  template: `<div>Hello, {{title}}</div>`
 })
 export class AppComponent {
-  title = "app";
+  title = "Abrar";
 }
--- hadiths/static/hadiths/js-angular/main.ts
+++ hadiths/static/hadiths/js-angular/main.ts
@@ -1,9 +1,9 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from "app/app.module";
+import { AppModule } from "app.module";
 
-enableProdMode();
+//enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
